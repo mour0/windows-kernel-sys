1d08f516dd4a817cb9cc61683eb0c88357fd98cc
mour0 <malentacchimaurizio19@gmail.com>
2023-07-10 03:59:18 +0200

    PsGetProcessWow64Process

--- src/ntoskrnl.rs
+++ src/ntoskrnl.rs
@@ -11,11 +11,21 @@ extern "C" {
     pub fn _IoGetCurrentIrpStackLocation(irp: PIRP) -> PIO_STACK_LOCATION;
     
     pub fn _KeQuerySystemTime(current_time: PLARGE_INTEGER);
+
+    pub fn _MmCopyVirtualMemory(source_process: PEPROCESS, source_address: PVOID, target_process: PEPROCESS, target_address: PVOID, buffer_size: SIZE_T, previous_mode: KPROCESSOR_MODE, return_size: PSIZE_T) -> NTSTATUS;
+
+    pub fn _PsGetProcessWow64Process(process: PEPROCESS) -> PVOID;
 }
 
 pub use self::_IoGetCurrentIrpStackLocation as IoGetCurrentIrpStackLocation;
 
 pub use self::_KeQuerySystemTime as KeQuerySystemTime;
+
+pub use self::_MmCopyVirtualMemory as MmCopyVirtualMemory;
+
+pub use self::IoGetCurrentProcess as PsGetCurrentProcess;
+
+pub use self::_PsGetProcessWow64Process as PsGetProcessWow64Process;
 
 
 #[macro_export]
@@ -37,4 +47,11 @@ macro_rules! NT_SUCCESS {
     ($status:expr) => {
         ($status as NTSTATUS) >= 0
     };
+}
+
+#[macro_export]
+macro_rules! CTL_CODE {
+    ($DeviceType:expr, $Function:expr, $Method:expr, $Access:expr) => {
+        ($DeviceType << 16) | ($Access << 14) | ($Function << 2) | $Method
+    }
 }
--- src/wrapper.c
+++ src/wrapper.c
@@ -7,3 +7,6 @@ PIO_STACK_LOCATION _IoGetCurrentIrpStackLocation(PIRP irp) {
 void _KeQuerySystemTime(PLARGE_INTEGER CurrentTime) {
 	return KeQuerySystemTime(CurrentTime);
 }
+
+
+NTKERNELAPI PVOID NTAPI PsGetProcessWow64Process(_In_ PEPROCESS Process);
